167ccfd5b6fadfa3e806b091cc1185119a4c7530
JensKristianRuben <Jenskristian79@gmail.com>
2026-06-05 10:33:19 +0200

    added first playwrgiht test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("My first test", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await expect(page).toHaveTitle(/Jens Kristian Ruben | Datamatiker med fokus på automatisering, udvikling og devops/)
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  const cvLink = page.getByRole('link', {name: "cv"});
+  await cvLink.click();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  await expect(page).toHaveURL("http://localhost:5173/cv/")
+})
